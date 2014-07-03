7ab6a637be2dd6ef32d964c09318f503c2d93884
plegall <plg@piwigo.org>
2014-07-03 13:47:18 +0000

    new robots VoilaBot and BLEXBot
    git-svn-id: http://piwigo.org/svn/extensions/no_stats_for_robots@28935 68402e56-0260-453c-a942-63ccdbb3a9ee

--- main.inc.php
+++ main.inc.php
@@ -13,8 +13,20 @@ if (!defined('PHPWG_ROOT_PATH'))
   die('Hacking attempt!');
 }
 
+$bots = array(
+  'Googlebot',
+  'bingbot',
+  'Baiduspider',
+  'yandex',
+  'AhrefsBot',
+  'msnbot',
+  'Slurp',
+  'BLEXBot',
+  'VoilaBot',
+  );
+
 if (isset($_SERVER["HTTP_USER_AGENT"])
-    and preg_match('/(Googlebot|bingbot|Baiduspider|yandex|AhrefsBot|msnbot|Slurp)/', $_SERVER['HTTP_USER_AGENT']))
+    and preg_match('/('.implode('|', $bots).')/', $_SERVER['HTTP_USER_AGENT']))
 {
   add_event_handler('pwg_log_allowed', create_function('', 'return false;'));
   add_event_handler('allow_increment_element_hit_count', create_function('$b', 'return false;'));
